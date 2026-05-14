95bfa7ffed11bb561ecd205e2fee7191d6d3d537
Petr Makhnev <51853996+i582@users.noreply.github.com>
2026-05-15 03:37:19 +0400

    feat(docs): add search

--- docs/src/app/provider.tsx
+++ docs/src/app/provider.tsx
@@ -1,8 +1,17 @@
 "use client";
 
+import SearchDialog from "@/components/search";
 import { RootProvider } from "fumadocs-ui/provider/next";
 import type { ReactNode } from "react";
 
 export function Provider({ children }: { children: ReactNode }) {
-  return <RootProvider>{children}</RootProvider>;
+  return (
+    <RootProvider
+      search={{
+        SearchDialog,
+      }}
+    >
+      {children}
+    </RootProvider>
+  );
 }
