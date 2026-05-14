d70bd54bd4a59cf4e0faf81e5d7b99f9c6695395
Petr Makhnev <51853996+i582@users.noreply.github.com>
2026-05-15 03:28:58 +0400

    feat(docs): files tree

--- docs/src/lib/mdx-components.tsx
+++ docs/src/lib/mdx-components.tsx
@@ -1,9 +1,13 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
+import { File, Files, Folder } from "@/components/Files";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    File,
+    Files,
+    Folder,
     ...components,
   };
 }
